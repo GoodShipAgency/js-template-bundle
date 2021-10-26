96800ccfaba46aa70cd9245dfac97aa0d1ea9306
cmubo <muberyc@gmail.com>
2021-10-26 13:07:07 +0100

    CHUR-472 fix: Now exports the filters function

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { initialiseMultiSelect } from './components/multi-select';
 export { dropoutMenu } from './components/dropout-menu';
 export { overlay, overlayButton } from './components/overlays';
 export { tabs } from './components/tabs';
+export { filters } from './components/filters';
