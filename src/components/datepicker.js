6802eaa24a3fbff891dffcdc7ab38c35f5f9d29e
callummubery <muberyc@gmail.com>
2022-01-20 14:04:49 +0000

    MID-550 fix: fixed datepicker function so disable class actually works

--- src/components/datepicker.js
+++ src/components/datepicker.js
@@ -2,15 +2,16 @@ import flatpickr from 'flatpickr';
 import { walkParents } from '../helpers/dom-walker';
 
 export function datepicker(id, options = {}) {
+    const subjectNode = document.querySelector(id);
+
+    // If disabled, return early
+    if (walkParents(subjectNode, null, ['js-disable-datepicker-init'])) return;
+
     options.altInput = options.altInput ? options.altInput : true;
     options.mode = options.mode ? 'range' : 'single';
 
-    let subjectNode = document.querySelector(id);
-    let slideoverContainer = subjectNode
-        ? walkParents(subjectNode, null, [
-              'js-slideout-container',
-              'js-disable-datepicker-init',
-          ])
+    const slideoverContainer = subjectNode
+        ? walkParents(subjectNode, null, ['js-slideout-container'])
         : null;
 
     if (slideoverContainer) {
