27611d8ed55f164dc565d2ba342aa15aeaa9688c
callummubery <muberyc@gmail.com>
2022-04-05 13:19:44 +0100

    feat(MID-568): Made a few improvements to the slideout form functionality - Added Slideout-form directive - slideout-form has new window events dispatched for more customisability - slideout-button disable on successful submission directive.

---
+++ src/directives/slideout-form.js
@@ -0,0 +1,64 @@
+import { htmxDispatch } from '../components/htmx';
+import { overlayFormResponseError } from '../components/overlays';
+import { dispatchWindowEvent } from '../helpers/dispatch-window-event';
+
+/**
+ * Used as part of the template bundles slideout-form component.
+ * @param Alpine
+ *
+ * Arguments that can be passed to directive
+ * formId - Required
+ * formName - Required
+ * remainOpenOnSuccess - Whether the slideout should remain open on a successful request.
+ */
+export const slideoutForm = function (Alpine) {
+    Alpine.directive('slideout-form', (el, { expression }, { evaluate }) => {
+        const { formId, formName, flowName, remainOpenOnSuccess } = evaluate(expression);
+
+        el.addEventListener('htmx:before-request', beforeRequest);
+        el.addEventListener('htmx:after-request', afterRequest);
+        el.addEventListener('htmx:response-error', responseError);
+        el.addEventListener('htmx:send-error', sendError);
+
+        function beforeRequest() {
+            el.querySelector(`button#${formId}_submit`).disabled = true;
+        }
+
+        function afterRequest(event) {
+            el.querySelector(`button#${formId}_submit`).disabled = false;
+
+            if (event.detail.successful) {
+                dispatchWindowEvent('slideout-form-success', el, {
+                    formId,
+                    formName,
+                    flowName,
+                    htmxDetails: event.detail,
+                });
+
+                htmxDispatch({ name: formName });
+
+                if (!remainOpenOnSuccess) {
+                    dispatchWindowEvent('slideout-close');
+                }
+            }
+        }
+
+        function responseError(event) {
+            dispatchWindowEvent('slideout-form-error', el, {
+                formId,
+                formName,
+                htmxDetails: event.detail,
+            });
+            overlayFormResponseError(event, formId);
+        }
+
+        function sendError(event) {
+            dispatchWindowEvent('slideout-form-error', el, {
+                formId,
+                formName,
+                htmxDetails: event.detail,
+            });
+            overlayFormResponseError(event, formId);
+        }
+    });
+};
--- src/index.js
+++ src/index.js
@@ -12,5 +12,8 @@ export { tabs } from './components/tabs';
 export { filters } from './components/filters';
 export { disableButton, disableFormsOnSubmit } from './helpers/disable-button';
 export { walkParents } from './helpers/dom-walker';
+export { dispatchWindowEvent } from './helpers/dispatch-window-event';
 export { unsavedNotice } from './directives/unsaved-notice';
 export { multiSelect } from './directives/multi-select';
+export { slideoutForm } from './directives/slideout-form';
+export { disableOnSuccess } from './directives/slideout-button';
